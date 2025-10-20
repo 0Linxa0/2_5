6bf802c2cb79f9157005ae540201b4281489bd2e
JINX <92461540+0Linxa0@users.noreply.github.com>
2025-10-20 07:51:02 +0000

    2_5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,14 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
 
-// import org.junit.jupiter.api.Test;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    for (int i=50; i<100; i++){
+      if(i%3==0){
+        System.out.println(i);
+      }
+    }
+      
   }
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+
 }

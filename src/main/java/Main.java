6f4469c8fc55b0ffb087606cf1897a0befff3bdc
JINX <92461540+0Linxa0@users.noreply.github.com>
2025-10-20 07:54:46 +0000

    2_5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@
 public class Main {
   public static void main(String[] args) {
     for (int i=50; i<101; i++){
-      if(i%3==0){
+      if(i%2 !=0){
         System.out.println(i);
       }
     }
